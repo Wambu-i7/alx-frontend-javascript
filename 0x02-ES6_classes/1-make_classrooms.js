2d225270a103c8c3ca6314c8a746a130dc45cb6a
Wambu-i7 <wambuindikiru@gmail.com>
2024-10-16 06:34:31 +0000

    new classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -5,5 +5,9 @@ import ClassRoom from './0-classroom';
  * @returns An array of {@link ClassRoom}s.
  */
 export default function initializeRooms() {
-  return [19, 20, 34].map((size) => new ClassRoom(size));
+  return [
+	  new ClassRoom(19),
+	  new ClassRoom(20),
+	  new ClassRoom(34)
+  	];
 }
